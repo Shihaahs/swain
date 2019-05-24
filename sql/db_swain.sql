ff970fa1c334bafc5c93da8c3075fe5434d4d91c
13756 <1375637251@qq.com>
2019-05-24 13:52:04 +0800

    還差報表

--- sql/db_swain.sql
+++ sql/db_swain.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50644
 File Encoding         : 65001
 
-Date: 2019-05-16 21:24:00
+Date: 2019-05-24 13:50:09
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -33,10 +33,9 @@ CREATE TABLE `machine` (
 -- ----------------------------
 -- Records of machine
 -- ----------------------------
-INSERT INTO `machine` VALUES ('1', '一号机', '1', '2', '0', '2019-05-09 19:57:57', '2019-05-16 10:35:12');
+INSERT INTO `machine` VALUES ('1', '一号机', '0', '2', '0', '2019-05-09 19:57:57', '2019-05-24 13:21:13');
 INSERT INTO `machine` VALUES ('2', '二号机', '1', '2', '0', '2019-05-09 19:57:57', '2019-05-12 00:23:33');
-INSERT INTO `machine` VALUES ('3', '三号机', '1', '2', '1', '2019-05-12 21:35:24', '2019-05-16 21:16:32');
-INSERT INTO `machine` VALUES ('4', '三号机', '1', '2', '0', '2019-05-12 21:37:20', '2019-05-16 21:16:38');
+INSERT INTO `machine` VALUES ('3', '三号机', '1', '2', '0', '2019-05-12 21:37:20', '2019-05-18 11:45:44');
 
 -- ----------------------------
 -- Table structure for `material`
@@ -51,7 +50,7 @@ CREATE TABLE `material` (
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `material_total` decimal(20,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '物料库存，单位kg',
   PRIMARY KEY (`material_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of material
@@ -59,6 +58,7 @@ CREATE TABLE `material` (
 INSERT INTO `material` VALUES ('1', '小麦', '2.00', '0', '2019-05-09 20:12:51', '2019-05-12 19:48:53', '1012.00');
 INSERT INTO `material` VALUES ('2', '橡胶', '100.00', '0', '2019-05-09 20:14:22', '2019-05-16 13:56:30', '1001.00');
 INSERT INTO `material` VALUES ('6', '玉米', '2.00', '0', '2019-05-12 21:44:07', '2019-05-13 00:08:31', '111.00');
+INSERT INTO `material` VALUES ('7', '油菜', '2.00', '0', '2019-05-18 10:44:19', '2019-05-18 10:44:19', '1000.00');
 
 -- ----------------------------
 -- Table structure for `product`
@@ -69,19 +69,20 @@ CREATE TABLE `product` (
   `product_user_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '生产员工id',
   `product_machine_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '生产机器id',
   `product_material_id` bigint(20) NOT NULL COMMENT '生产原料id',
-  `product_material_weight` decimal(2,0) unsigned NOT NULL DEFAULT '0' COMMENT '生产原料重量',
+  `product_material_weight` decimal(10,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '生产原料重量',
   `product_out_name` varchar(255) NOT NULL COMMENT '产出物名称',
-  `product_out_weight` decimal(2,0) unsigned NOT NULL DEFAULT '0' COMMENT '产出物重量',
+  `product_out_weight` decimal(10,0) unsigned NOT NULL DEFAULT '0' COMMENT '产出物重量',
   `is_delete` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '逻辑删除，0-存在，1-已被删除',
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`product_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of product
 -- ----------------------------
-INSERT INTO `product` VALUES ('1', '2', '1', '1', '50', '面粉', '25', '0', '2019-05-16 09:05:24', '2019-05-16 19:38:12');
+INSERT INTO `product` VALUES ('1', '2', '1', '1', '50.00', '面粉', '25', '0', '2019-05-16 09:05:24', '2019-05-21 16:17:37');
+INSERT INTO `product` VALUES ('2', '4', '1', '6', '12.00', '玉米油', '55', '0', '2019-05-21 16:51:04', '2019-05-24 13:44:50');
 
 -- ----------------------------
 -- Table structure for `repair`
@@ -97,12 +98,15 @@ CREATE TABLE `repair` (
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `repair_comment` varchar(255) NOT NULL DEFAULT '' COMMENT '维修备注',
   PRIMARY KEY (`repair_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of repair
 -- ----------------------------
-INSERT INTO `repair` VALUES ('1', '1', '1', '50.00', '0', '2019-05-16 16:12:21', '2019-05-16 19:34:39', '开关坏了');
+INSERT INTO `repair` VALUES ('1', '1', '1', '50.00', '0', '2019-05-16 16:12:21', '2019-05-16 22:29:05', '开关坏了');
+INSERT INTO `repair` VALUES ('2', '1', '1', '55.00', '0', '2019-05-18 11:21:14', '2019-05-22 21:57:29', '传送带坏了');
+INSERT INTO `repair` VALUES ('3', '2', '1', '60.00', '0', '2019-05-18 11:49:45', '2019-05-18 12:57:45', '传送带坏了');
+INSERT INTO `repair` VALUES ('4', '1', '1', '120.00', '0', '2019-05-21 16:13:57', '2019-05-21 16:14:24', '开关坏了');
 
 -- ----------------------------
 -- Table structure for `user`
@@ -117,7 +121,7 @@ CREATE TABLE `user` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
@@ -126,3 +130,4 @@ INSERT INTO `user` VALUES ('1', 'admin', 'admin', '0', '0', '2019-05-09 19:29:01
 INSERT INTO `user` VALUES ('2', '张三', '123456', '1', '0', '2019-05-09 19:29:01', '2019-05-11 11:16:14');
 INSERT INTO `user` VALUES ('3', '李四', '123123', '1', '0', '2019-05-16 19:36:22', '2019-05-16 19:36:50');
 INSERT INTO `user` VALUES ('4', '王五', '123321', '1', '0', '2019-05-16 19:36:35', '2019-05-16 19:37:02');
+INSERT INTO `user` VALUES ('6', '是傻傻', '123123', '1', '0', '2019-05-18 16:34:24', '2019-05-18 16:34:24');
